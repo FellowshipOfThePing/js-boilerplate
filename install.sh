91b1b1ccf85f44c80ae2a00d043ac83bceeeebf6
Jeremy Binder <jeremysbinder@gmail.com>
2022-08-24 19:02:57 -0400

    Basic install file working

--- install.sh
+++ install.sh
@@ -6,9 +6,36 @@ ql_cyan='\033[1;36m'
 ql_green='\033[1;32m'
 ql_no_color='\033[0m'
 
-# Download utils file to local machine
-curl -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/FellowshipOfThePing/js-boilerplate/master/.eslintignore" \
---output "./.eslintignore"
+# Download file function
+downloadfile() {
+    if [ -f "./$1" ]; then
+        echo "$1 already exists."
+    else 
+        prefix='https://raw.githubusercontent.com/FellowshipOfThePing/js-boilerplate/master/'
+        curl -s -H 'Cache-Control: no-cache' "${prefix}$1" --output "./$1"
+        echo -e "${ql_cyan}=> Downloaded $1. ${ql_no_color}";
+    fi    
+}
+
+# Download github folder
+mkdir -p ./.github
+downloadfile ".github/pull_request_template.md"
+
+# Download vscode folder
+mkdir -p ./.vscode
+downloadfile ".vscode/tasks.json"
+downloadfile ".vscode/extensions.json"
+downloadfile ".vscode/javascript.code-snippets"
+downloadfile ".vscode/settings.json"
+
+# Download other files
+downloadfile ".eslintignore"
+downloadfile ".eslintrc.js"
+downloadfile ".gitignore"
+downloadfile ".prettierignore"
+downloadfile ".prettierrc"
+downloadfile "babel.config.js"
+downloadfile "CODEOWNERS"
 
 # Prompt user
 echo "";
